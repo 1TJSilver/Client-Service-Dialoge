9701b8cbb5ebf7bb3c0d71ee9889f5f8ce47d5e0
John Terve <89539449797@mail.ru>
2022-08-05 21:31:44 +0300

    Init rep

---
+++ src/main/java/ClientMain.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        String host = "netology.homework";
+        int port = 8765;
+        try (Socket client = new Socket(host, port)){
+            Scanner scanner = new Scanner(System.in);
+            PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(client.getInputStream()));
+            System.out.println(reader.readLine());
+            writer.println(scanner.nextLine());
+            System.out.println(reader.readLine());
+            System.out.println(reader.readLine());
+            writer.println(scanner.nextLine());
+            System.out.println(reader.readLine());
+            System.out.println(reader.readLine());
+            writer.println(scanner.nextLine());
+            System.out.println(reader.readLine());
+            System.out.println(reader.readLine());
+            writer.println(scanner.nextLine());
+            System.out.println(reader.readLine());
+            System.out.println(reader.readLine());
+
+        } catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
